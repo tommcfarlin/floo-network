78b723c410634e82a80108c30889f678527f8e8b
tommcfarlin <tom@tommcfarlin.com>
2026-04-07 10:37:06 -0400

    chore: set Supabase anon key in config

--- config.js
+++ config.js
@@ -7,6 +7,6 @@
 
 const FLOO_CONFIG = Object.freeze({
   SUPABASE_URL: 'https://oxjlmlekjtuvkfgvzajb.supabase.co',
-  SUPABASE_ANON_KEY: 'REPLACE_WITH_ANON_KEY',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94amxtbGVranR1dmtmZ3Z6YWpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUwNjgyMDMsImV4cCI6MjA5MDY0NDIwM30.G0MXnl4kvPF4sX5nyV_0SqJsoxg9y2Osm9lwHB8B-fg',
   GOOGLE_CLIENT_ID: '225405562171-2d765ie44rvr8662bsgcl6ok464067ha.apps.googleusercontent.com',
 });
